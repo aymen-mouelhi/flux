aabc9f8001089a4a28f30c227ab6c806d373ffa8
Aymen MOUELHI <aymen.mouelhi@sap.com>
2015-03-14 23:19:22 +0100

    Todo Application
    Change-Id: I8a8ca34b649bc02cd7eb201ac9c71e8aa2f8262d

---
+++ Todo/js/app.js
@@ -0,0 +1,11 @@
+/**
+ * Created by I060307 on 14/03/2015.
+ */
+var React = require('react');
+
+var TodoApp = require('./components/TodoApp.react');
+
+React.render(
+<TodoApp />,
+    document.getElementById('todoapp')
+);
