85746fe58f8a969ed547217a624bd5656621d731
Martin Blaha <martin@organisedminds.com>
2015-03-26 08:03:02 +0100

    Show port number when running the server

--- app.js
+++ app.js
@@ -5,6 +5,8 @@
 var express = require('express');
 var morgan = require('morgan');
 var app = express();
+var port = process.env.PORT || 5000;
 
 app.use(express.static(__dirname + '/Pinboard'));
-app.listen(process.env.PORT || 5000);
+console.log('Running on localhost:' + port)
+app.listen(port);
